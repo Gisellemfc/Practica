d76e541363ab0e7975903fa9ee2451705b3cf4c8
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-05 17:39:00 -0400

    Importar data json

--- practica/src/app/componentes/home/home.component.ts
+++ practica/src/app/componentes/home/home.component.ts
@@ -1,3 +1,4 @@
+import juegos from 'src/assets/data/juegos.json';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  listaJuegos: any = juegos;
 
   constructor() { }
 
